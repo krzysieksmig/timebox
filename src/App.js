39018c070e11b04174de6fbe7b56dd52651bdd17
Krzysiek <krzysieksmig@gmail.com>
2021-05-19 15:43:57 +0200

    Added App.js file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import EditableTimebox from './components/EditableTimebox/EditableTimebox';
+
+function App(){
+    return(
+        <div className="App">
+            <EditableTimebox/>
+        </div>
+    )
+}
+
+export default App;
