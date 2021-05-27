23ba59ad6ab6289d8de04b47f7f5815e2f1b1942
Krzysiek <krzysieksmig@gmail.com>
2021-05-27 14:04:25 +0200

    Added function to check min and max value

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 function ProgressBar({progress = 15 , trackRemaining = false}){
     return(
         trackRemaining ? 
@@ -10,6 +11,16 @@ function ProgressBar({progress = 15 , trackRemaining = false}){
             <div style={{width: `${progress}%`}}></div>
         </div>
     )
+}
+
+function numberBetween(props, propName, componentName){
+    if (props[propName] < 0 || props[propName] > 100){
+        return new Error(`Invalid prop ${propName} in component ${componentName}. Value must be beetwen 0-100`)
+    }
+}
+
+ProgressBar.propTypes = {
+    progress: numberBetween
 }
 
 export default ProgressBar
