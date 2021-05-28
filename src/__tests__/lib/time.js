631dd386e8053f5c10ec21cf9be0b11a19ae9707
Krzysiek <krzysieksmig@gmail.com>
2021-05-28 13:46:46 +0200

    tests for time conver

--- src/__tests__/lib/time.js
+++ src/__tests__/lib/time.js
@@ -1,36 +1,36 @@
 import convertSecondsToTimer from '../../lib/time'
 describe("convertSecondsToTimer", ()=>{
-    test('works for 30 seconds', ()=>{
+    test('returns 30 seconds for 30 seconds', ()=>{
         expect(
             convertSecondsToTimer(30)
         ).toEqual([0,0,30,0])
     })
 
-    test('works for 60 seconds', ()=>{
+    test('returns 1 minute for 60 seconds', ()=>{
         expect(
             convertSecondsToTimer(60)
         ).toEqual([0,1,0,0])
     })
 
-    test('works for 120 seconds', ()=>{
+    test('returns 0 hours 2 minutes 0 seconds 0 mseconds for 120 seconds', ()=>{
         expect(
             convertSecondsToTimer(120)
         ).toEqual([0,2,0,0])
     })
 
-    test('works for 7200 seconds', ()=>{
+    test('returns 2 hours 0 minutes 0 seconds 0 mseconds for 7200 seconds', ()=>{
         expect(
             convertSecondsToTimer(7200)
         ).toEqual([2,0,0,0])
     })
 
-    test('works for 5400 seconds', ()=>{
+    test('returns 1 hours 30 minutes 0 seconds 0 mseconds for 5400 seconds', ()=>{
         expect(
             convertSecondsToTimer(5400)
         ).toEqual([1,30,0,0])
     })
 
-    test('works for 14400 seconds', ()=>{
+    test('returns 4 hours 0 minutes 0 seconds 0mseconds for 14400 seconds', ()=>{
         expect(
             convertSecondsToTimer(14400)
         ).toEqual([4,0,0,0])
